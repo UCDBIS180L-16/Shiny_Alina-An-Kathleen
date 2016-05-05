d50b3eecc0f5853d0378b825cde820e443420e5d
bis180l_local <klfurtado@ucdavis.edu>
2016-05-05 15:34:49 -0700

    KF Completed Practice Apps

---
+++ Practice_KF/ui.R
@@ -0,0 +1,29 @@
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage( #create the overall page
+  
+  # Application title
+  titlePanel("Iris Data"),
+  
+  # Some helpful information
+  helpText("This application creates a boxplot to show difference between",
+           "iris species.  Please use the radio box below to choose a trait",
+           "for plotting"),
+  
+  # Sidebar with a radio box to input which species will be plotted
+  sidebarLayout(
+    sidebarPanel(
+      radioButtons("Species", #the input variable that the value will go into
+                   "Choose a species to display:",
+                   c("setosa",
+                     "versicolor",
+                     "virginica")
+      )),
+    
+    # Show a plot of the generated distribution
+    mainPanel(plotOutput("boxPlot")
+    )
+  )
+))
+
