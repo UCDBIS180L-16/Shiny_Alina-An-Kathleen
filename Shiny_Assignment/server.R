9066cd3a8a48dde073e7e860347c9a44069769ab
austinan <ahmgnuyen@ucdavis.edu>
2016-05-05 15:43:48 -0700

    An Nguyen - created R scripts for assignment

---
+++ Shiny_Assignment/server.R
@@ -0,0 +1,26 @@
+
+# 5/5/2016 started
+# need to upload rice data
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+
+shinyServer(function(input, output) {
+
+  output$distPlot <- renderPlot({
+
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2]
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+
+  })
+
+})
